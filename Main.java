38c3ae6f6e7852633db4da67fa3e934535059d40
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-11-07 19:27:39 +0900

    [add]関数

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 
 class Main {
     public static void main(String args[]) {
-        System.out.println("Hello World!");
+        int num = 10;
+        String str = "test";
+
+        System.out.println("Hello World!" + str);
+        System.out.println(num);
     }
 }
