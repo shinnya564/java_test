05b61113b51b9a2ab51940af358a0227610afd0e
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-11-10 11:41:44 +0900

    [update]Sample_if条件分岐追加

--- Sample_if.java
+++ Sample_if.java
@@ -1,9 +1,11 @@
 class Sample_if {
   public static void main(String[] args) {
-    int percent = 50;
+    int percent = 3;
 
     if (percent >= 50) {
       System.out.println("傘を持っていきましょう");
+    } else if (percent >= 30) {
+      System.out.println("折り畳み傘を持っていきましょう");
     } else {
       System.out.println("傘は必要ありません");
     }
