d6c04d8b242653e9844fa51f52f647417306f08a
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-11-10 11:21:14 +0900

    [add]sample_if

---
+++ Sample_if.java
@@ -0,0 +1,11 @@
+class Sample_if {
+  public static void main(String[] args) {
+    int percent = 50;
+
+    if (percent >= 50) {
+      System.out.println("傘を持っていきましょう");
+    } else {
+      System.out.println("傘は必要ありません");
+    }
+  }
+}
