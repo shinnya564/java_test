03c59dddb98451518b0537f45762f828d8614fb1
荒木　信哉 <shinnya@MacBook-Pro.local>
2020-11-10 11:35:52 +0900

    [add]Sample3_1

---
+++ Sample3_1.java
@@ -0,0 +1,21 @@
+class Sample3_1 {
+  public static void main(String[] args) {
+    boolean a = true;
+    char b = 'A';
+    byte c = 0;
+    short d = 32767;
+    int e = 2147483647;
+    long f = 1000000000L;
+    float g = 15;
+    double h = 555.55;
+
+    System.out.println("変数aの値は" + a);
+    System.out.println("変数bの値は" + b);
+    System.out.println("変数cの値は" + c);
+    System.out.println("変数dの値は" + d);
+    System.out.println("変数eの値は" + e);
+    System.out.println("変数fの値は" + f);
+    System.out.println("変数gの値は" + g);
+    System.out.println("変数hの値は" + h);
+  }
+}
